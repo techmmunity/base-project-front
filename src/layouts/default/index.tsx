12262a53c2f45e5d922f6974fe7a9bf4934b8ac4
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-07 15:47:16 -0300

    Initial Commit

---
+++ src/layouts/default/index.tsx
@@ -0,0 +1,1 @@
+export const DefaultLayout: React.FC = ({ children }) => <>{children}</>;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from "next/app";
+
+import { Head } from "components/Head";
+
+import { LayoutWrapper } from "layouts/wrapper";
+
+import GlobalStyle from "styles/GlobalStyle";
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+	<>
+		<Head />
+		<LayoutWrapper {...pageProps}>
+			<Component {...pageProps} />
+		</LayoutWrapper>
+		<GlobalStyle />
+	</>
+);
+
+export default MyApp;
