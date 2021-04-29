d48358f75571055a94a415746f4e7234f2335a85
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-29 09:55:09 -0300

    Add multilanguage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
+import { GetStaticProps } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
 import { Container } from "styles/pages/Home";
 
-const Home = () => <Container>Hello World</Container>;
+const Home = () => {
+	const { t } = useTranslation();
+
+	return <Container>{t("message")}</Container>;
+};
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+	return {
+		props: {
+			...(await serverSideTranslations(locale as string)),
+		},
+	};
+};
