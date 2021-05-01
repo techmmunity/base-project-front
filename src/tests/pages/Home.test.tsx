b822bdaea18500323e380d4577a97cef71df8992
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-30 21:10:43 -0300

    Add tests

--- src/tests/pages/Home.test.tsx
+++ src/tests/pages/Home.test.tsx
@@ -3,12 +3,14 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Home from "pages";
 
-it("should get the container id", () => {
-	const { getByTestId } = render(<Home />);
-	expect(getByTestId("message")).toBeInTheDocument();
-});
+describe("Pages > Home", () => {
+	it("should get the container id", () => {
+		const { getByTestId } = render(<Home />);
+		expect(getByTestId("message")).toBeInTheDocument();
+	});
 
-it("should get the container text", () => {
-	const { getByText } = render(<Home />);
-	expect(getByText("message")).toBeInTheDocument();
+	it("should get the container text", () => {
+		const { getByText } = render(<Home />);
+		expect(getByText("message")).toBeInTheDocument();
+	});
 });
