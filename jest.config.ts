8a997960fd06deb96ad248267b13560240d538d0
Henrique Leite <henriqueleite616@gmail.com>
2021-05-02 16:11:55 -0300

    Add Tests Validation Workflow (#18)

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-	collectCoverageFrom: ["pages/**/*.tsx"],
-	coverageDirectory: "../coverage",
-	clearMocks: true,
-	moduleDirectories: ["node_modules", "src"],
-	rootDir: "src",
-	setupFiles: ["<rootDir>/tests/setup.ts"],
-};
