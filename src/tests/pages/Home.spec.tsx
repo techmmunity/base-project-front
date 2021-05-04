273d0ec18565479dcacfaefca256888a81a2a9ee
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-05-03 22:16:48 -0300

    Fix jest

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,16 +1,18 @@
 import { render } from "@testing-library/react";
-
 import "@testing-library/jest-dom/extend-expect";
+
 import Home from "pages";
 
 describe("Pages > Home", () => {
 	it("should get the container id", () => {
 		const { getByTestId } = render(<Home />);
+
 		expect(getByTestId("message")).toBeInTheDocument();
 	});
 
 	it("should get the container text", () => {
 		const { getByText } = render(<Home />);
+
 		expect(getByText("message")).toBeInTheDocument();
 	});
 });
