b1ec754406a85ee13d9ce68259c81bf89df8e9d7
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-05-05 10:34:03 -0300

    Add dynamic meta (#25)

--- src/components/HeadContent/index.tsx
+++ src/components/HeadContent/index.tsx
@@ -1,8 +1,12 @@
-export const HeadContent = () => (
+interface Props {
+	htmlLang?: string;
+}
+
+export const HeadContent: React.FC<Props> = ({ htmlLang = "en" }) => (
 	<>
 		<meta name="og:site_name" content="Base Project Front" />
 		<meta name="og:url" content="https://example.com" />
-		<meta name="og:locale" content="en-us" />
+		<meta name="og:locale" content={htmlLang} />
 		<link rel="cannonical" href="https://example.com" />
 	</>
 );
