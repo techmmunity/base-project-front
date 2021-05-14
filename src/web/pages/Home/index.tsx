ca4500d1bfdbd08cab60eddc6d2f4959b0462ebc
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-05-13 21:36:14 -0300

    Fix home page

--- src/web/pages/Home/index.tsx
+++ src/web/pages/Home/index.tsx
@@ -1,8 +1,10 @@
 import { useTranslation } from "next-i18next";
 
+import { FCWithLayout } from "types/interfaces/layout";
+
 import { Container } from "./styles";
 
-export const HomePage: React.FC = () => {
+export const Home: FCWithLayout = () => {
 	const { t } = useTranslation();
 
 	return <Container>{t("message")}</Container>;
