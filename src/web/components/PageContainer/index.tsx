8d312be4ed3b6f6ed976eb369f9b394cb7bdaf05
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-05-29 14:04:30 -0300

    Add page container

---
+++ src/web/components/PageContainer/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+
+export const PageContainer: React.FC = ({ children }) => (
+	<Container>{children}</Container>
+);
---
+++ src/web/components/PageContainer/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+	@media (max-width: 768px) {
+		padding: 0 2rem;
+	}
+`;
