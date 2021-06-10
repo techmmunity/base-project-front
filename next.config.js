685b71988f6aaebdf31280023ddb43759dabac8c
Alessandro Ramos <70819804+alessandroramos13@users.noreply.github.com>
2021-06-10 08:32:15 -0300

    Fix files with errors (#57)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { i18n } = require("./next-i18next.config");
 
 module.exports = {
