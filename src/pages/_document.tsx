2acfb12048975d666e41de4b02bf4aad2f52630f
Alessandro Ramos <toprime13@gmail.com>
2021-06-10 10:00:21 -0300

    Add multiple translations files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,13 +10,18 @@ import { ServerStyleSheet } from "styled-components";
 
 import { HeadContent } from "web/components/HeadContent";
 
-const getHtmlLang = (locale: string) => {
+import enUSSystem from "assets/translations/en-US/system.json";
+import ptBRSystem from "assets/translations/pt-BR/system.json";
+
+import { GetSystemInformationResult } from "types/interfaces/system";
+
+const getSystemInformation = (locale: string): GetSystemInformationResult => {
 	switch (locale) {
 		case "pt-BR":
-			return "pt";
+			return ptBRSystem as GetSystemInformationResult;
 		case "en-US":
 		default:
-			return "en";
+			return enUSSystem as GetSystemInformationResult;
 	}
 };
 
@@ -49,16 +54,16 @@ export default class MyDocument extends Document {
 
 	public render() {
 		const { locale } = this.props.__NEXT_DATA__;
-		const htmlLang = getHtmlLang(locale as string);
+		const { html, head } = getSystemInformation(locale as string);
 
 		return (
-			<Html lang={htmlLang}>
+			<Html lang={html.lang}>
 				<Head>
 					<link
 						href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
 						rel="stylesheet"
 					/>
-					<HeadContent htmlLang={htmlLang} />
+					<HeadContent ogLocale={head.metas.ogLocale} />
 				</Head>
 				<body>
 					<Main />
