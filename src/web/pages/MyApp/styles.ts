68558d9e9165b881956598a2c87dbc84f99e8c3c
Alessandro Ramos <toprime13@gmail.com>
2021-11-15 19:25:21 -0300

    Update Media Querys

--- src/web/pages/MyApp/styles.ts
+++ src/web/pages/MyApp/styles.ts
@@ -79,19 +79,31 @@ export const GlobalStyle = createGlobalStyle`
 
 	@media (max-width: 768px) {
     html {
+      font-size: 60%;
+    }
+  }
+
+	@media (max-width: 425px) {
+    html {
       font-size: 57.5%;
+    }
+  }
+
+	@media (max-width: 375px) {
+    html {
+      font-size: 55%;
     }
   }
 
 	@media (max-width: 320px) {
     html {
-      font-size: 55.5%;
+      font-size: 52.5%;
     }
   }
 
 	@media (max-width: 280px) {
     html {
-      font-size: 52.5%;
+      font-size: 50%;
     }
   }
 `;
