23d43cecd4df236b9250269c66fb52df3bdf6c66
Alessandro Ramos <toprime13@gmail.com>
2021-11-15 13:19:14 -0300

    Update Fonts

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -2,43 +2,38 @@ import { css } from "styled-components";
 
 export const FONTS = {
 	heading1: css`
-		font-size: 5.6rem;
-		line-height: 6rem;
-		font-weight: 700;
+		font-size: 6.4rem;
+		line-height: 7.4rem;
+		font-weight: bold;
 	`,
 	heading2: css`
 		font-size: 4.8rem;
-		line-height: 5.4rem;
-		font-weight: 700;
+		line-height: 5.8rem;
+		font-weight: bold;
 	`,
 	heading3: css`
 		font-size: 3.2rem;
-		line-height: 3.8rem;
-		font-weight: 400;
+		line-height: 4.2rem;
+		font-weight: bold;
 	`,
 	heading4: css`
-		font-size: 1.8rem;
-		line-height: 2.8rem;
-		font-weight: 400;
+		font-size: 2.4rem;
+		line-height: 3.4rem;
+		font-weight: 500;
 	`,
 	text1: css`
 		font-size: 1.6rem;
 		line-height: 2.6rem;
-		font-weight: 400;
+		font-weight: normal;
 	`,
 	text2: css`
 		font-size: 1.4rem;
 		line-height: 2.4rem;
-		font-weight: 400;
+		font-weight: normal;
 	`,
 	text3: css`
 		font-size: 1.2rem;
 		line-height: 2rem;
-		font-weight: 400;
-	`,
-	text4: css`
-		font-size: 1rem;
-		line-height: 1.8rem;
-		font-weight: 400;
+		font-weight: normal;
 	`,
 };
